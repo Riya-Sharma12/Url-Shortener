f7ece3b18ac695d8083330f99cd388665e7abc54
Riya-Sharma12 <sharmariya0163@gmail.com>
2025-04-19 15:55:13 +0530

    checking

--- src/pages/RedirectLinkPage.jsx
+++ src/pages/RedirectLinkPage.jsx
@@ -1,10 +1,53 @@
-// import React from 'react'
-// import {useParams} from "react-router-dom";
-// import { storeClicks } from '@/db/apiUrls';
+// // import React from 'react'
+// // import {useParams} from "react-router-dom";
+// // import { storeClicks } from '@/db/apiUrls';
+// // import useFetch from '@/hooks/useFetch';
+// // import {BarLoader} from "react-spinners";
+// // import {getLongUrl} from "@/db/apiUrls";
+// // import {useEffect} from "react";
+
+// // const RedirectLinkPage = () => {
+// //   const {id} = useParams();
+
+// //   const {loading, data, fn} = useFetch(getLongUrl, id);
+
+// //   const {loading: loadingStats, fn: fnStats} = useFetch(storeClicks, {
+// //     id: data?.id,
+// //     originalUrl: data?.original_url,
+// //   });
+
+// //   useEffect(() => {
+// //     fn();
+// //   }, [id]);
+
+// //   useEffect(() => {
+// //     if (!loading && data) {
+// //       fnStats();
+// //     }
+// //   }, [loading]);
+
+// //   if (loading || loadingStats) {
+// //     return (
+// //       <>
+// //         <BarLoader width={"100%"} color="#36d7b7" />
+// //         <br />
+// //         Redirecting...
+// //       </>
+// //     );
+// //   }
+
+// //   return null;
+// // };
+
+// // export default RedirectLinkPage;
+
+
+// import React, { useEffect } from 'react';
+// import { useParams, useNavigate } from "react-router-dom";
+// import { getLongUrl } from '@/db/apiUrls';
+// import { storeClicks } from '@/db/apiClicks';
 // import useFetch from '@/hooks/useFetch';
-// import {BarLoader} from "react-spinners";
-// import {getLongUrl} from "@/db/apiUrls";
-// import {useEffect} from "react";
+// import { BarLoader } from "react-spinners";
 
 // const RedirectLinkPage = () => {
 //   const {id} = useParams();
@@ -18,12 +61,13 @@
 
 //   useEffect(() => {
 //     fn();
-//   }, [id]);
+//   }, []);
 
 //   useEffect(() => {
 //     if (!loading && data) {
 //       fnStats();
 //     }
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
 //   }, [loading]);
 
 //   if (loading || loadingStats) {
@@ -42,6 +86,8 @@
 // export default RedirectLinkPage;
 
 
+
+
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from "react-router-dom";
 import { getLongUrl } from '@/db/apiUrls';
@@ -50,25 +96,30 @@ import useFetch from '@/hooks/useFetch';
 import { BarLoader } from "react-spinners";
 
 const RedirectLinkPage = () => {
-  const {id} = useParams();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  const {loading, data, fn} = useFetch(getLongUrl, id);
+  // Fetch the long URL from the database based on the shortcode
+  const { loading, data, fn } = useFetch(getLongUrl, id);
 
-  const {loading: loadingStats, fn: fnStats} = useFetch(storeClicks, {
+  // Store click event for the shortened link
+  const { loading: loadingStats, fn: fnStats } = useFetch(storeClicks, {
     id: data?.id,
     originalUrl: data?.original_url,
   });
 
   useEffect(() => {
-    fn();
-  }, []);
+    fn(); // Fetch long URL on component mount
+  }, [id]);
 
   useEffect(() => {
     if (!loading && data) {
-      fnStats();
+      fnStats(); // Store the click event if the data is fetched
+      // Redirect the user to the long URL
+      window.location.href = data.original_url;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading]);
+  }, [loading, data]);
 
   if (loading || loadingStats) {
     return (
